d46d1e9e4ef4f34d5b3714e179dfb5f8ea3d6d13
Blake DeCarvalho <malbolge@tuta.io>
2019-09-24 12:06:14 -0400

    Init commit

---
+++ Drivehealth_with_SMTP.ps1
@@ -0,0 +1,31 @@
+﻿$fullread=Get-PhysicalDisk | Sort Size | FT FriendlyName, Size, MediaType, HealthStatus, OperationalStatus -AutoSize | Out-String
+$readerr=Get-PhysicalDisk | Sort Size | Select-Object HealthStatus
+if($readerr.HealthStatus -like 'Unhealthy' -Or $readerr.HealthStatus -like 'Warning') {
+
+    function sendMail{
+
+         Write-Host "Sending Email"
+
+        $EmailFrom = "FROMEMAIL”
+
+        $EmailTo = “TOEMAIL”
+
+        $Subject = “DRIVE HEALTH ALERT”
+
+        $Body = “DRIVE HEALTH WARNING $fullread”
+
+        $SMTPServer = “SMTPSERVER”
+
+        $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 2525)
+
+        $SMTPClient.EnableSsl = $true
+
+        $SMTPClient.Credentials = New-Object System.Net.NetworkCredential(“USERNAME”, “PASSWORD”);
+
+        $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+ 
+    }
+    
+    #Calling function
+    sendMail
+}
---
+++ Ping_Check_with_SMTP.ps1
@@ -0,0 +1,31 @@
+﻿$fullread=Test-Connection IPADDRESSHERE -Count 5 | Out-String
+$readerr=Test-Connection IPADDRESSHERE -Count 5 -Quiet
+if($readerr -eq $false) {
+
+    function sendMail{
+
+         Write-Host "Sending Email"
+
+        $EmailFrom = “FROMEMAIL”
+
+        $EmailTo = “TOEMAIL”
+
+        $Subject = “Ping Failure”
+
+        $Body = “Ping test of HOSTNAME $fullread”
+
+        $SMTPServer = “SMTPSERVERADDRESS”
+
+        $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 2525)
+
+        $SMTPClient.EnableSsl = $true
+
+        $SMTPClient.Credentials = New-Object System.Net.NetworkCredential(“USERNAME”, “PASSWORD”);
+
+        $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+ 
+    }
+    
+    #Calling function
+    sendMail
+}
